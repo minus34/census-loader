4a2e5d8d56a1bb6f6c06e732ccece4690955f5c6
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-05-28 14:10:45 +1000

    new display bdys schema ready

--- load-census.py
+++ load-census.py
@@ -469,9 +469,9 @@ def create_display_boundaries_2(pg_cur, settings):
         column_list = list()
         column_list.append("id text NOT NULL PRIMARY KEY")
         column_list.append("name text NOT NULL")
-        column_list.append("area double precision NOT NULL")
-        column_list.append("b1 double precision NOT NULL")
-        column_list.append("geom geometry(MultiPolygon, 4326) NOT NULL")
+        column_list.append("area double precision NULL")
+        column_list.append("population double precision NOT NULL")
+        column_list.append("geom geometry(MultiPolygon, 4326) NULL")
 
         # for stat in stats:
         #     column_list.append("{0} double precision".format(stat[0].lower()))
@@ -485,31 +485,45 @@ def create_display_boundaries_2(pg_cur, settings):
         sql = "".join(create_table_list).format(pg_schema, pg_table, settings['pg_user'])
         create_sql_list.append(sql)
 
+        # get population field and table
+        if boundary_name[:1] == "i":
+            pop_stat = "i3"
+            pop_table = "i01a"
+        elif settings["census_year"] == "2011":
+            pop_stat = "b3"
+            pop_table = "b01"
+        else:
+            pop_stat = "g3"
+            pop_table = "g01"
+
         insert_into_list = list()
 
         insert_into_list.append("INSERT INTO {0}.{1}".format(pg_schema, pg_table))
-        insert_into_list.append("".format())
-        insert_into_list.append("".format())
-        insert_into_list.append("".format())
-        insert_into_list.append("".format())
-        insert_into_list.append("".format())
+        insert_into_list.append("SELECT {0} AS id, {1} AS name, SUM({2}) AS area, {3},"
+                                .format(id_field, name_field, area_field, pop_stat))
+        insert_into_list.append("ST_Transform(ST_Multi(ST_Union(ST_SimplifyVW(ST_Transform(geom, 3577), {0}))), 4326)".format(tolerance,))
+        insert_into_list.append("FROM {0}.{1} AS bdy".format(settings['boundary_schema'], input_pg_table))
+        insert_into_list.append("INNER JOIN {0}.{1}_{2} AS tab".format(settings['data_schema'], boundary_name, pop_table))
+        insert_into_list.append("ON bdy.{0} = tab.{1}".format(id_field, settings["region_id_field"]))
+        insert_into_list.append("GROUP BY {0}, {1}, {2}".format(id_field, name_field, pop_stat))
 
-        sql = "INSERT INTO {0}.{1} " \
-              "SELECT bdy.{5} AS id, bdy.{6} AS name, SUM(bdy.{7}) AS area, tab.b1" \
-              "ST_Transform(ST_Multi(ST_Union(ST_SimplifyVW(ST_Transform(bdy.geom, 3577), {4}))), 4326) " \
-              "FROM {2}.{3} " \
-              "INNER JOIN " \
-              "GROUP BY id, name" \
-            .format(pg_schema, pg_table, settings['boundary_schema'], input_pg_table,
-                    tolerance, id_field, name_field, area_field)
+        # sql = "INSERT INTO {0}.{1} " \
+        #       "SELECT bdy.{5} AS id, bdy.{6} AS name, SUM(bdy.{7}) AS area, tab.b1" \
+        #       "ST_Transform(ST_Multi(ST_Union(ST_SimplifyVW(ST_Transform(bdy.geom, 3577), {4}))), 4326) " \
+        #       "FROM {2}.{3} " \
+        #       "INNER JOIN " \
+        #       "GROUP BY id, name" \
+        #     .format(pg_schema, pg_table, settings['boundary_schema'], input_pg_table,
+        #             tolerance, id_field, name_field, area_field)
 
+        sql = " ".join(insert_into_list)
         insert_sql_list.append(sql)
 
-        # vacuum_sql_list.append("VACUUM ANALYZE {0}.{1}".format(pg_schema, pg_table))
+        vacuum_sql_list.append("VACUUM ANALYZE {0}.{1}".format(pg_schema, pg_table))
 
     utils.multiprocess_list("sql", create_sql_list, settings, logger)
-    # utils.multiprocess_list("sql", insert_sql_list, settings, logger)
-    # utils.multiprocess_list("sql", vacuum_sql_list, settings, logger)
+    utils.multiprocess_list("sql", insert_sql_list, settings, logger)
+    utils.multiprocess_list("sql", vacuum_sql_list, settings, logger)
 
     logger.info("\t- Step 2 of 2 : display census boundaries created : {0}".format(datetime.now() - start_time))
 
