cf6ae9da2030894934dd35b10e2b0e5ac6c49b73
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-07-19 15:25:40 +1000

    testing parallel pg dumnps

--- supporting-files/dump-census-schemas.sh
+++ supporting-files/dump-census-schemas.sh
@@ -3,3 +3,7 @@
 /Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n census_2016_data -p 5432 -U postgres -f ~/tmp/census_2016_data.dmp
 /Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n census_2016_bdys -p 5432 -U postgres -f ~/tmp/census_2016_bdys.dmp
 /Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n census_2016_web -p 5432 -U postgres -f ~/tmp/census_2016_web.dmp
+
+#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_data -p 5432 -U postgres -f ~/tmp/census_2016_data
+#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_bdys -p 5432 -U postgres -f ~/tmp/census_2016_bdys
+#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_web -p 5432 -U postgres -f ~/tmp/census_2016_web
