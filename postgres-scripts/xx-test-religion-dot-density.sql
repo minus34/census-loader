aac3d8c595932ec4382b6f927794731ac3fc52cd
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-12-05 19:07:14 +1100

    fix memory alloc issue

--- postgres-scripts/xx-test-religion-dot-density.sql
+++ postgres-scripts/xx-test-religion-dot-density.sql
@@ -11,179 +11,184 @@
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_population;
 WITH temp_sa1 AS (
-  SELECT tab.G5462::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5462::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5462::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_population
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_population OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_population ADD CONSTRAINT dots_population_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_population ADD CONSTRAINT dots_population_pkey PRIMARY KEY (id);
 CREATE INDEX dots_population_geom_idx ON census_2016_sandpit.dots_population USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_population CLUSTER ON dots_population_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_population;
+ANALYZE census_2016_sandpit.dots_population;
 
 
 -- no religion
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_non_religious;
 WITH temp_sa1 AS (
-  SELECT tab.G5447::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5447::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5447::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_non_religious
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_non_religious OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_non_religious ADD CONSTRAINT dots_non_religious_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_non_religious ADD CONSTRAINT dots_non_religious_pkey PRIMARY KEY (id);
 CREATE INDEX dots_non_religious_geom_idx ON census_2016_sandpit.dots_non_religious USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_non_religious CLUSTER ON dots_non_religious_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_non_religious;
+ANALYZE census_2016_sandpit.dots_non_religious;
 
 
 -- christianity
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_christian;
 WITH temp_sa1 AS (
-  SELECT tab.G5423::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5423::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5423::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_christian
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_christian OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_christian ADD CONSTRAINT dots_christian_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_christian ADD CONSTRAINT dots_christian_pkey PRIMARY KEY (id);
 CREATE INDEX dots_christian_geom_idx ON census_2016_sandpit.dots_christian USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_christian CLUSTER ON dots_christian_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_christian;
+ANALYZE census_2016_sandpit.dots_christian;
 
 
 -- islam
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_islam;
 WITH temp_sa1 AS (
-  SELECT tab.G5429::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5429::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5429::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_islam
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_islam OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_islam ADD CONSTRAINT dots_islam_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_islam ADD CONSTRAINT dots_islam_pkey PRIMARY KEY (id);
 CREATE INDEX dots_islam_geom_idx ON census_2016_sandpit.dots_islam USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_islam CLUSTER ON dots_islam_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_islam;
+ANALYZE census_2016_sandpit.dots_islam;
 
 
 -- buddhism
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_buddhism;
 WITH temp_sa1 AS (
-  SELECT tab.G5363::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5363::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5363::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_buddhism
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_buddhism OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_buddhism ADD CONSTRAINT dots_buddhism_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_buddhism ADD CONSTRAINT dots_buddhism_pkey PRIMARY KEY (id);
 CREATE INDEX dots_buddhism_geom_idx ON census_2016_sandpit.dots_buddhism USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_buddhism CLUSTER ON dots_buddhism_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_buddhism;
+ANALYZE census_2016_sandpit.dots_buddhism;
 
 
 -- hinduism
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_hinduism;
 WITH temp_sa1 AS (
-  SELECT tab.G5426::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5426::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5426::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_hinduism
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_hinduism OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_hinduism ADD CONSTRAINT dots_hinduism_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_hinduism ADD CONSTRAINT dots_hinduism_pkey PRIMARY KEY (id);
 CREATE INDEX dots_hinduism_geom_idx ON census_2016_sandpit.dots_hinduism USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_hinduism CLUSTER ON dots_hinduism_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_hinduism;
+ANALYZE census_2016_sandpit.dots_hinduism;
 
 
 -- judaism
 
 DROP TABLE IF EXISTS census_2016_sandpit.dots_judaism;
 WITH temp_sa1 AS (
-  SELECT tab.G5432::integer as val,
-    ST_Buffer(ST_SnapToGrid(bdy.geom, 0.0001), 0.0) As geom
+  SELECT tab.region_id AS id, tab.G5432::integer as val,
+    bdy.geom
     FROM census_2016_bdys.sa1_2016_aust AS bdy
     INNER JOIN census_2016_data.sa1_G14 AS tab ON bdy.sa1_7dig16 = tab.region_id
     WHERE tab.G5432::integer > 0
     -- AND bdy.ste_code16 IN ('1', '8')
     AND NOT ST_IsEmpty(geom)
 )
-SELECT row_number() OVER () as gid,
+SELECT id,
   ST_GeneratePoints(geom, val) As geom
   INTO census_2016_sandpit.dots_judaism
   FROM temp_sa1
   WHERE ST_Area(ST_Envelope(geom)) > 0.0;
 
 ALTER TABLE census_2016_sandpit.dots_judaism OWNER to postgres;
-ALTER TABLE census_2016_sandpit.dots_judaism ADD CONSTRAINT dots_judaism_pkey PRIMARY KEY (gid);
+ALTER TABLE census_2016_sandpit.dots_judaism ADD CONSTRAINT dots_judaism_pkey PRIMARY KEY (id);
 CREATE INDEX dots_judaism_geom_idx ON census_2016_sandpit.dots_judaism USING gist (geom);
 ALTER TABLE census_2016_sandpit.dots_judaism CLUSTER ON dots_judaism_geom_idx;
 
-VACUUM ANALYZE census_2016_sandpit.dots_judaism;
+ANALYZE census_2016_sandpit.dots_judaism;
+
+-- select Count(*) as cnt from census_2016_sandpit.dots_judaism;
+
+-- SELECT * FROM census_2016_bdys.sa1_2016_aust
+--   WHERE ST_Area(ST_Buffer(ST_SnapToGrid(geom, 0.0001), 0.0))/ST_Area(ST_Envelope(ST_Buffer(ST_SnapToGrid(geom, 0.0001), 0.0))) < 0.0001;
