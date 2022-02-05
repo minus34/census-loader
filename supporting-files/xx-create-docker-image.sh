6b1cf8e8918d9e9b63df41d944c9a150b328e0d8
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-02-05 18:26:33 +1100

    fixed dmp file paths

--- supporting-files/xx-create-docker-image.sh
+++ supporting-files/xx-create-docker-image.sh
@@ -8,13 +8,13 @@ echo "build census-loader docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 cd ${SCRIPT_DIR}/../docker
-docker build --squash --tag minus34/censusloader:latest --tag minus34/censusloader:2016 --no-cache  --no-cache --build-arg BASE_URL="https://minus34.com/opendata/census-2016" .
+docker build --squash --tag minus34/censusloader:latest --tag minus34/censusloader:2016 --no-cache  --no-cache --build-arg CENSUS_YEAR="2016" BASE_URL="https://minus34.com/opendata/census-2016" .
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "build census-loader GDA2020 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-docker build --squash --tag minus34/censusloader:latest-gda2020 --tag minus34/censusloader:2016-gda2020 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/census-2016-gda2020" .
+docker build --squash --tag minus34/censusloader:latest-gda2020 --tag minus34/censusloader:2016-gda2020 --no-cache --build-arg CENSUS_YEAR="2016" BASE_URL="https://minus34.com/opendata/census-2016-gda2020" .
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "push images to Docker Hub"
