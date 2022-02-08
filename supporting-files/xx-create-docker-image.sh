44b512034f28929af43ca49cea168034ba2800ea
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-02-08 18:59:26 +1100

    added 2011 census to container build

--- supporting-files/xx-create-docker-image.sh
+++ supporting-files/xx-create-docker-image.sh
@@ -12,11 +12,11 @@ docker build --squash --tag minus34/censusloader:2016 --no-cache  --no-cache --b
 docker build --squash --tag minus34/censusloader:2011 --no-cache  --no-cache --build-arg CENSUS_YEAR="2011" --build-arg BASE_URL="https://minus34.com/opendata/census-2011" .
 
 
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "build census-loader GDA2020 docker image"
-echo "---------------------------------------------------------------------------------------------------------------------"
-
-docker build --squash --tag minus34/censusloader:latest-gda2020 --tag minus34/censusloader:2016-gda2020 --no-cache --build-arg CENSUS_YEAR="2016" --build-arg BASE_URL="https://minus34.com/opendata/census-2016-gda2020" .
+#echo "---------------------------------------------------------------------------------------------------------------------"
+#echo "build census-loader GDA2020 docker image"
+#echo "---------------------------------------------------------------------------------------------------------------------"
+#
+#docker build --squash --tag minus34/censusloader:latest-gda2020 --tag minus34/censusloader:2016-gda2020 --no-cache --build-arg CENSUS_YEAR="2016" --build-arg BASE_URL="https://minus34.com/opendata/census-2016-gda2020" .
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "push images to Docker Hub"
