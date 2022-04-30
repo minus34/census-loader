a4f262e2a7c18816b699a4f182a2be78bd8c2e81
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-04-30 15:50:31 +1000

    updates for 2021 data

--- supporting-files/dump-census-schemas.sh
+++ supporting-files/dump-census-schemas.sh
@@ -7,17 +7,14 @@ SECONDS=0*
 CENSUS_YEAR="2021"
 OUTPUT_FOLDER="/Users/$(whoami)/tmp"
 
-DATA_PATH="${OUTPUT_FOLDER}/census_${CENSUS_YEAR}_data"
-BDYS_PATH="${OUTPUT_FOLDER}/census_${CENSUS_YEAR}_bdys"
-
 DATA_SCHEMA="census_${CENSUS_YEAR}_data"
 BDYS_SCHEMA="census_${CENSUS_YEAR}_bdys"
 WEB_SCHEMA="census_${CENSUS_YEAR}_web"
 
 # dump schemas to backup files
-#/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${DATA_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${DATA_SCHEMA}.dmp
-/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${BDYS_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${BDYS_SCHEMA}.dmp
-#/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${WEB_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${WEB_SCHEMA}.dmp
+#/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${DATA_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${DATA_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${BDYS_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${BDYS_SCHEMA}.dmp
+#/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${WEB_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${WEB_SCHEMA}.dmp
 
 
 # OPTIONAL - copy files to AWS S3 and allow public read access (requires awscli installed)
