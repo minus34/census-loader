12eb492e5ac4ba2a5c50c8c8a71ef0ac4420f4f1
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-04-30 13:36:18 +1000

    updates for 2021 data

--- supporting-files/dump-census-schemas.sh
+++ supporting-files/dump-census-schemas.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-# set this to taste - NOTE: can't use "~" for your home folder
-output_folder="/Users/hugh.saalmans/tmp"
+#!/usr/bin/env bash
 
-# set to 2011 or 2016
-census_year="2016"
+SECONDS=0*
+
+CENSUS_YEAR="2021"
+OUTPUT_FOLDER="/Users/$(whoami)/tmp"
+
+DATA_PATH="${OUTPUT_FOLDER}/census_${CENSUS_YEAR}_data"
+BDYS_PATH="${OUTPUT_FOLDER}/census_${CENSUS_YEAR}_bdys"
+
+DATA_SCHEMA="census_${CENSUS_YEAR}_data"
+BDYS_SCHEMA="census_${CENSUS_YEAR}_bdys"
+WEB_SCHEMA="census_${CENSUS_YEAR}_web"
 
 # dump schemas to backup files
-/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -d geo -n census_${census_year}_data -p 5432 -U postgres -f ${output_folder}/census_${census_year}_data.dmp
-/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -d geo -n census_${census_year}_bdys -p 5432 -U postgres -f ${output_folder}/census_${census_year}_bdys.dmp
-/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -d geo -n census_${census_year}_web -p 5432 -U postgres -f ${output_folder}/census_${census_year}_web.dmp
+#/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${DATA_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${DATA_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${BDYS_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${BDYS_SCHEMA}.dmp
+#/Applications/Postgres.app/Contents/Versions/10/bin/pg_dump -Fc -j 4 -d geo -n ${WEB_SCHEMA} -p 5432 -U postgres -f ${output_folder}/${WEB_SCHEMA}.dmp
 
-# dump using multiple workers (not tested - doesn't necessarily work)
-#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_data -p 5432 -U postgres -f ~/tmp/census_2016_data
-#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_bdys -p 5432 -U postgres -f ~/tmp/census_2016_bdys
-#/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fd -j 4 -d geo -n census_2016_web -p 5432 -U postgres -f ~/tmp/census_2016_web
 
-## OPTIONAL - copy files to AWS S3 and allow public read access (requires awscli installed)
-#cd ${output_folder}
-#
-#for f in census_${census_year}_*.dmp;
-#  do
-#    aws --profile=default s3 cp --storage-class REDUCED_REDUNDANCY ./${f} s3://minus34.com/opendata/census-${census_year}/${f};
-#    aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/census-${census_year}/${f}
-#  done
+# OPTIONAL - copy files to AWS S3 and allow public read access (requires awscli installed)
+cd ${OUTPUT_FOLDER}
+
+for f in census_${CENSUS_YEAR}_*.dmp;
+  do
+    aws --profile=default s3 cp --storage-class REDUCED_REDUNDANCY ./${f} s3://minus34.com/opendata/census-${CENSUS_YEAR}/${f};
+    aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/census-${CENSUS_YEAR}/${f}
+  done
