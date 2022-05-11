5ebaf71495607936e3543cc5a5a609ce2569e4d5
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-05-11 10:52:28 +1000

    added fix for duplicated bdy code prefixes

---
+++ postgres-scripts/xx_bdy_code_prefix_fix.sql
@@ -0,0 +1,10 @@
+
+-- only run these ONCE!
+
+update census_2016_bdys.sed_2016_aust
+    set sed_code16 = substring(sed_code16, 4, length(sed_code16))
+from census_2016_bdys.sed_2016_aust;
+
+update census_2016_bdys.ced_2016_aust
+    set ced_code16 = substring(ced_code16, 4, length(ced_code16))
+from census_2016_bdys.ced_2016_aust;
