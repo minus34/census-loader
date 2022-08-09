6fce24aee6e1227cd4956f2519223b1e3a430d82
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-08-09 16:15:06 +1000

    testing vector thinning

--- testing/region_simplify_testing.sh
+++ testing/region_simplify_testing.sh
@@ -7,10 +7,11 @@
 #git clone https://github.com/eurostat/RegionSimplify.git
 #cd RegionSimplify
 #mvn package
-#
-#BDYS_PATH="/Users/$(whoami)/tmp/census_2021_bdys"
-#
-#mkdir -p "${BDYS_PATH}/thinned"
+
+
+BDYS_PATH="/Users/$(whoami)/tmp/census_2021_bdys"
+
+mkdir -p "${BDYS_PATH}/thinned"
 
 # load environment with GDAL
 conda deactivate
@@ -21,16 +22,15 @@ cd $GIT_HOME/eurostat/RegionSimplify
 # thin each layer and output to geopackage
 for dataset in "sa1_2021_aust_gda94"
 do
-  query="SELECT * FROM census_2021_bdys_gda94.${dataset} WHERE geom IS NOT NULL"
+  echo "Exporting ${dataset} to GeoPackage"
+  ogr2ogr -f GPKG "${BDYS_PATH}/${dataset}.gpkg" \
+  PG:"host='localhost' dbname='geo' user='postgres' password='password' port='5432'" -sql "SELECT * FROM census_2021_bdys_gda94.${dataset} WHERE geom IS NOT NULL"
 
-  ogr2ogr -f GeoPackage "${output_folder}/${dataset}.gpkg" \
-  PG:"host=localhost dbname=geo user=postgres password=password port=5432" -sql ${query}
-
-#	for scaleM in "5" "10" "20"
-#	do
-#    	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
-#		java -Xmx12g -Xms4g -jar ./target/RegionSimplify-1.4.0-SNAPSHOT.jar -i "/Users/s57405/tmp/census_2021_bdys/"$dataset".gpkg" -o "/Users/s57405/tmp/census_2021_bdys/thinned/"$dataset"_"$scaleM"m.gpkg" -s $scaleM"000000"
-#	done
+	for scaleM in "5" "10" "20"
+	do
+    echo "Thinning ${dataset} at 1:${scaleM}000000"
+		java -Xmx12g -Xms4g -jar ./target/RegionSimplify-1.4.0-SNAPSHOT.jar -i "${BDYS_PATH}/${dataset}.gpkg" -o "${BDYS_PATH}/${dataset}_${scaleM}m.gpkg" -s "${scaleM}000000"
+	done
 done
 
 
