946ec9b60c0c547639d15c65bcfa320e072116fb
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-08-09 16:18:13 +1000

    testing vector thinning

--- testing/region_simplify_testing.sh
+++ testing/region_simplify_testing.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## SETUP - download Git repo and build locally (requires Java and Maven)
+## ONE TIME SETUP - download Git repo and build locally (requires Java and Maven)
 #cd ${GIT_HOME}
 #mkdir -p eurostat
 #cd eurostat
@@ -8,18 +8,18 @@
 #cd RegionSimplify
 #mvn package
 
-
 BDYS_PATH="/Users/$(whoami)/tmp/census_2021_bdys"
 
+# create output path
 mkdir -p "${BDYS_PATH}/thinned"
 
 # load environment with GDAL
 conda deactivate
 conda activate geo
 
+# thin each layer and output to geopackage
 cd $GIT_HOME/eurostat/RegionSimplify
 
-# thin each layer and output to geopackage
 for dataset in "sa1_2021_aust_gda94"
 do
   echo "Exporting ${dataset} to GeoPackage"
@@ -32,16 +32,3 @@ do
 		java -Xmx12g -Xms4g -jar ./target/RegionSimplify-1.4.0-SNAPSHOT.jar -i "${BDYS_PATH}/${dataset}.gpkg" -o "${BDYS_PATH}/${dataset}_${scaleM}m.gpkg" -s "${scaleM}000000"
 	done
 done
-
-
-
-#java -Xmx8g -Xms4g -jar /Users/s57405/Downloads/RegionSimplify/target/RegionSimplify-1.4.0-SNAPSHOT.jar \
-#-i "/Users/s57405/tmp/census_2021_bdys/ASGS_2021_Main_Structure_GDA2020.gpkg" \
-#-o "/Users/s57405/tmp/census_2021_bdys/thinned/test.gpkg"
-#
-#
-#java -Xmx8g -Xms4g -jar /Users/s57405/Downloads/RegionSimplify/target/RegionSimplify-1.4.0-SNAPSHOT.jar \
-#-i "/Users/s57405/tmp/census_2021_bdys/test.shp" \
-#-o "/Users/s57405/tmp/census_2021_bdys/thinned/test.shp"
-
-
