226b6b544406e721c680bb515d2c634369f57019
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-08-10 14:04:57 +1000

    testing vector thinning

--- testing/region_simplify_testing.sh
+++ testing/region_simplify_testing.sh
@@ -20,15 +20,20 @@ conda activate geo
 # thin each layer and output to geopackage
 cd $GIT_HOME/eurostat/RegionSimplify
 
-for dataset in "ste_2021_aust_gda94" "sa4_2021_aust_gda94" "sa3_2021_aust_gda94" "sa2_2021_aust_gda94" "sa1_2021_aust_gda94"
+#"ste_2021_aust_gda94" "sa4_2021_aust_gda94" "sa3_2021_aust_gda94" "sa2_2021_aust_gda94" "sa1_2021_aust_gda94"
+
+for dataset in "sa4_2021_aust_gda94"
 do
   echo "Exporting ${dataset} to Shapefile"
-  ogr2ogr "${BDYS_PATH}/${dataset}.shp" \
+  ogr2ogr -f GPKG "${BDYS_PATH}/${dataset}.gpkg" \
   PG:"host='localhost' dbname='geo' user='postgres' password='password' port='5432'" -sql "SELECT * FROM census_2021_bdys_gda94.${dataset} WHERE geom IS NOT NULL"
 
-	for scaleM in "1" "3" "5" "7" "9"˚
+	for scaleM in "1" "3" "5" "7" "9"
 	do
     echo "Thinning ${dataset} at 1:${scaleM}000000"
 		java -Xmx12g -Xms4g -jar ./target/RegionSimplify-1.4.0-SNAPSHOT.jar -i "${BDYS_PATH}/${dataset}.shp" -o "${BDYS_PATH}//thinned/${dataset}_${scaleM}m.gpkg" -s "${scaleM}000000"
+
+    # load results into PostGIS
+    ogr2ogr -f "PostgreSQL" -overwrite -nlt MULTIPOLYGON -nln "testing.${dataset}_${scaleM}m" PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${BDYS_PATH}//thinned/${dataset}_${scaleM}m.gpkg"
 	done
 done
---
+++ testing/xx_testing.sql
@@ -0,0 +1,57 @@
+
+-- +---------+-----------+----------+----------+----------+
+-- |num_geoms|coord_count|avg_coords|min_coords|max_coords|
+-- +---------+-----------+----------+----------+----------+
+-- |10       |1839295    |204366    |2575      |625504    |
+-- +---------+-----------+----------+----------+----------+
+select count(*) as num_geoms,
+       count(distinct sa4_code_2021) as num_ids,
+       sum(st_npoints(geom)) as coord_count,
+       avg(st_npoints(geom))::integer as avg_coords,
+       min(st_npoints(geom)) as min_coords,
+       max(st_npoints(geom)) as max_coords
+from census_2021_bdys_gda94.sa4_2021_aust_gda94
+where geom is not null
+;
+
+-- +---------+-----------+----------+----------+----------+
+-- |num_geoms|coord_count|avg_coords|min_coords|max_coords|
+-- +---------+-----------+----------+----------+----------+
+-- |9        |34018      |3780      |164       |11494     |
+-- +---------+-----------+----------+----------+----------+
+select count(*) as num_geoms,
+       count(distinct sa4_code_2) as num_ids,
+       sum(st_npoints(geometry)) as coord_count,
+       avg(st_npoints(geometry))::integer as avg_coords,
+       min(st_npoints(geometry)) as min_coords,
+       max(st_npoints(geometry)) as max_coords
+from testing.sa4_2021_aust_gda94_1m;
+
+select count(*) as num_geoms,
+       sum(st_npoints(geometry)) as coord_count,
+       avg(st_npoints(geometry))::integer as avg_coords,
+       min(st_npoints(geometry)) as min_coords,
+       max(st_npoints(geometry)) as max_coords
+from testing.sa4_2021_aust_gda94_9m;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select count(*) as num_geoms,
+       sum(st_npoints(geom))
+--        sum(st_npoints(st_simplifyvw(geom, 0.0000001))),
+--        sum(st_npoints(st_simplify(geom, 0.00005))),
+--        sum(st_npoints(st_simplifypreservetopology(geom, 0.00005)))
+from census_2021_bdys_gda94.ste_2021_aust_gda94
+;
