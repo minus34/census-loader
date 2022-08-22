c88a9064f4d227991680941271b8622e273c6a6d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-08-22 11:21:32 +1000

    testing FlatGeoBuf performance

--- testing/region_simplify_testing.sh
+++ testing/region_simplify_testing.sh
@@ -26,10 +26,10 @@ cd $GIT_HOME/eurostat/RegionSimplify
 
 # "ste_2021_aust_gda94" "sa4_2021_aust_gda94" "sa3_2021_aust_gda94" "sa2_2021_aust_gda94" "sa1_2021_aust_gda94"
 
-#scale=800000
-#
-#for dataset in "ste_2021_aust_gda94" "sa4_2021_aust_gda94" "sa3_2021_aust_gda94" "sa2_2021_aust_gda94" "sa1_2021_aust_gda94"
-#do
+scale=800000
+
+for dataset in "ste_2021_aust_gda94" "sa4_2021_aust_gda94" "sa3_2021_aust_gda94" "sa2_2021_aust_gda94" "sa1_2021_aust_gda94"
+do
 #  echo "Exporting ${dataset} to GeoPackage and removing NULL geometries"
 #  ogr2ogr -f GPKG "${BDYS_PATH}/${dataset}.gpkg" \
 #  PG:"host='localhost' dbname='geo' user='postgres' password='password' port='5432'" \
@@ -37,7 +37,7 @@ cd $GIT_HOME/eurostat/RegionSimplify
 #
 #  echo "Thinning ${dataset} at 1:${scale}"
 #
-#  filename="${dataset}_${scale}"
+  filename="${dataset}_${scale}"
 #
 #  java -Xmx12g -Xms4g -jar ./target/RegionSimplify-1.4.0-SNAPSHOT.jar -i "${BDYS_PATH}/${dataset}.gpkg" \
 #  -o "${OUTPUT_FOLDER}/${filename}.gpkg" -s "${scale}" -omcn 256 > /dev/null
@@ -46,14 +46,14 @@ cd $GIT_HOME/eurostat/RegionSimplify
 #  ogr2ogr -f "PostgreSQL" -overwrite -lco geometry_name=geom -nlt MULTIPOLYGON -nln "testing.${filename}" \
 #  PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${OUTPUT_FOLDER}/${filename}.gpkg"
 #
-#  # output to FlatGeoBuf
-#  ogr2ogr -f FlatGeobuf "${OUTPUT_FOLDER}/${filename}.fgb" \
-#  PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "testing.${filename}"
-#
-#  scale=$((scale / 2))
-#done
+  # output to FlatGeoBuf
+  ogr2ogr -s_srs EPSG:4283 -t_srs EPSG:4326 -f FlatGeobuf "${OUTPUT_FOLDER}/${filename}.fgb" \
+  PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "testing.${filename}"
+
+  scale=$((scale / 2))
+done
 
 
 # copy output files to AWS
-aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/census-2021/geopackage --exclude "*" --include "*.gpkg" --acl public-read
+#aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/census-2021/geopackage --exclude "*" --include "*.gpkg" --acl public-read
 aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/census-2021/flatgeobuf --exclude "*" --include "*.fgb" --acl public-read
