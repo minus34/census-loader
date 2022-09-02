bc244c01adb2f46c2369053eaad3f111f6f2e2c3
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-09-02 16:57:08 +1000

    improved environment build

--- supporting-files/xx_setup_conda_env.sh
+++ supporting-files/xx_setup_conda_env.sh
@@ -2,48 +2,53 @@
 
 # --------------------------------------------------------------------------------------------------------------------
 
+ENV_NAME=geo
 PYTHON_VERSION="3.10"
 
 # --------------------------------------------------------------------------------------------------------------------
 
 echo "-------------------------------------------------------------------------"
-echo "Creating new Conda Environment 'geo'"
+echo "Creating new Conda Environment '${ENV_NAME}'"
 echo "-------------------------------------------------------------------------"
 
-# WARNING - removes existing environment
+# deactivate current environment and start base env (in case you just deactivated it) - lazy method
 conda deactivate
-conda env remove --name geo
+conda activate base
 
-# update Conda platform & install Mamba (much faster package installer)
+# WARNING - removes existing environment
+conda env remove --name ${ENV_NAME}
+
+# update Conda base environment
 conda update -y conda
-conda update -y -n base conda
-conda install -y mamba -n base -c conda-forge
 
 # Create Conda environment
-conda create -y -n geo python=${PYTHON_VERSION}
+conda create -y -n ${ENV_NAME} python=${PYTHON_VERSION}
 
 # activate and setup env
-conda activate geo
+conda activate ${ENV_NAME}
 conda env config vars set JAVA_HOME="/usr/local/opt/openjdk@11"
 conda config --env --add channels conda-forge
 conda config --env --set channel_priority strict
 
 # reactivate for env vars to take effect
-conda activate geo
+conda activate ${ENV_NAME}
 
-# install geospatial packages
-mamba install -y -c conda-forge gdal pygeos pyarrow dask-geopandas openpyxl psycopg2 geoalchemy2 rasterio boto3
-conda activate geo
+# install Mamba (faster package installer)
+conda install -y -c conda-forge mamba
+
+# install ${ENV_NAME}spatial packages
+mamba install -y -c conda-forge gdal pygeos pyarrow dask-geopandas openpyxl psycopg geoalchemy2 rasterio boto3
+conda activate ${ENV_NAME}
 
 # --------------------------
 # extra bits
 # --------------------------
 
 ## activate env
-#conda activate geo
+#conda activate ${ENV_NAME}
 
 ## shut down env
 #conda deactivate
 
 ## delete env permanently
-#conda env remove --name geo
+#conda env remove --name ${ENV_NAME}
