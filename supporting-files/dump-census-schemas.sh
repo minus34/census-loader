92ab02dbec02c41b24412ce4c05b29def1ca67dd
Hugh Saalmans <hugh.saalmans@gmail.com>
2023-02-12 13:08:47 +1100

    Changed Postgres version

--- supporting-files/dump-census-schemas.sh
+++ supporting-files/dump-census-schemas.sh
@@ -13,10 +13,10 @@ BDYS_2020_SCHEMA="census_${CENSUS_YEAR}_bdys_gda2020"
 WEB_SCHEMA="census_${CENSUS_YEAR}_web"
 
 # dump schemas to backup files
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${DATA_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${DATA_SCHEMA}.dmp
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${BDYS_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${BDYS_SCHEMA}.dmp
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${BDYS_2020_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${BDYS_2020_SCHEMA}.dmp
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_dump -Fc -d geo -n ${WEB_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${WEB_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/14/bin/pg_dump -Fc -d geo -n ${DATA_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${DATA_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/14/bin/pg_dump -Fc -d geo -n ${BDYS_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${BDYS_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/14/bin/pg_dump -Fc -d geo -n ${BDYS_2020_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${BDYS_2020_SCHEMA}.dmp
+/Applications/Postgres.app/Contents/Versions/14/bin/pg_dump -Fc -d geo -n ${WEB_SCHEMA} -p 5432 -U postgres -f ${OUTPUT_FOLDER}/${WEB_SCHEMA}.dmp
 
 
 # OPTIONAL - copy files to AWS S3 and allow public read access (requires awscli installed)
