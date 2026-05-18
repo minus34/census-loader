1b8605a20b9fffd9525301271138842303e9b5a3
Hugh Saalmans <hugh.saalmans@gmail.com>
2026-05-18 18:34:04 +1000

    added 2016 bdys restore

--- supporting-files/restore-census-schemas.sh
+++ supporting-files/restore-census-schemas.sh
@@ -14,6 +14,10 @@ curl --insecure https://minus34.com/opendata/census-2016/census_2016_data.dmp --
 /Applications/Postgres.app/Contents/Versions/16/bin/pg_restore -Fc -d geo -p 5432 -U postgres ./census_2016_data.dmp
 rm ./census_2016_data.dmp
 
+curl --insecure https://minus34.com/opendata/census-2016/census_2016_bdys.dmp --output ./census_2016_bdys.dmp
+/Applications/Postgres.app/Contents/Versions/16/bin/pg_restore -Fc -d geo -p 5432 -U postgres ./census_2016_bdys.dmp
+rm ./census_2016_bdys.dmp
+
 curl --insecure https://minus34.com/opendata/census-2021/census_2021_data.dmp --output ./census_2021_data.dmp
 /Applications/Postgres.app/Contents/Versions/16/bin/pg_restore -Fc -d geo -p 5432 -U postgres ./census_2021_data.dmp
 rm ./census_2021_data.dmp
